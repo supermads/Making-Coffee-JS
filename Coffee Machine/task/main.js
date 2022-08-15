870139b735ad791cab929e01a599c7df1e7f4e96
supermads <madisonsfahey@gmail.com>
2022-08-15 11:24:05 -0700

    Stage 3

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,11 +1,36 @@
-function calculateNeededIngredients (){
-    const input = require('sync-input');
-    let coffee = input("Write how many cups of coffee you will need:");
-    let water = coffee * 200;
-    let milk = coffee * 50;
-    let beans = coffee * 15;
-    console.log(`For ${coffee} cups of coffee you will need:`);
-    console.log(`${water}  ml of water\n${milk} ml of milk\n${beans} g of coffee beans`);
+function calculateCoffeeAvailable (waterInMachine, milkInMachine, beansInMachine){
+    let waterPerCup = 200;
+    let milkPerCup = 50;
+    let beansPerCup = 15;
+
+    let coffeeAvailable = 0;
+
+    while (waterInMachine >= waterPerCup  & milkInMachine >= milkPerCup & beansInMachine >= beansPerCup){
+        coffeeAvailable++;
+        waterInMachine -= waterPerCup;
+        milkInMachine -= milkPerCup;
+        beansInMachine -= beansPerCup;
+    };
+
+    return coffeeAvailable;
 }
 
-calculateNeededIngredients();
+function main() {
+    const input = require('sync-input');
+    let waterInMachine = input("Write how many ml of water the coffee machine has:");
+    let milkInMachine = input("Write how many ml of milk the coffee machine has:");
+    let beansInMachine = input("Write how many grams of coffee beans the coffee machine has:");
+    let coffee = input("Write how many cups of coffee you will need:");
+
+    let coffeeAvailable = calculateCoffeeAvailable(waterInMachine, milkInMachine, beansInMachine);
+
+    if (coffeeAvailable == coffee) {
+        console.log("Yes, I can make that amount of coffee");
+    } else if (coffeeAvailable > coffee) {
+        console.log(`Yes, I can make that amount of coffee (and even ${coffeeAvailable - coffee} more than that)`);
+    } else {
+        console.log(`No, I can make only ${coffeeAvailable} cups of coffee`);
+    }
+}
+
+main();
