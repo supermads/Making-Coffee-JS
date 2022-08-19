fb9741dc0fc672698c6eecf78919beb949ef1e56
supermads <madisonsfahey@gmail.com>
2022-08-19 15:57:28 -0700

    Stage 4

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,36 +1,62 @@
-function calculateCoffeeAvailable (waterInMachine, milkInMachine, beansInMachine){
-    let waterPerCup = 200;
-    let milkPerCup = 50;
-    let beansPerCup = 15;
+const input = require('sync-input');
 
-    let coffeeAvailable = 0;
+function printState() {
+    console.log("The coffee machine has:");
+    console.log(`${water} ml of water`);
+    console.log(`${milk} ml of milk`);
+    console.log(`${beans} g of coffee beans`);
+    console.log(`${cups} disposable cups`);
+    console.log(`\$${money} of money`);
+}
 
-    while (waterInMachine >= waterPerCup  & milkInMachine >= milkPerCup & beansInMachine >= beansPerCup){
-        coffeeAvailable++;
-        waterInMachine -= waterPerCup;
-        milkInMachine -= milkPerCup;
-        beansInMachine -= beansPerCup;
-    };
+function buy() {
+    let drinkChoice = parseInt(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")) - 1;
+    // drinkIngredients = [[waterRequired, milkRequired, beansRequired, drinkCost], ...]
+    let drinkIngredients = [[250, 0, 16, 4], [350, 75, 20, 7], [200, 100, 12, 6]];
 
-    return coffeeAvailable;
+    cups -= 1;
+    water -= drinkIngredients[drinkChoice][0];
+    milk -= drinkIngredients[drinkChoice][1];
+    beans -= drinkIngredients[drinkChoice][2];
+    money += drinkIngredients[drinkChoice][3];
+}
+
+function fill() {
+    let waterToAdd = parseInt(input("Write how many ml of water you want to add:"));
+    let milkToAdd = parseInt(input("Write how many ml of milk you want to add:"));
+    let beansToAdd = parseInt(input("Write how many grams of coffee beans you want to add:"));
+    let cupsToAdd = parseInt(input("Write how many disposable coffee cups you want to add:"));
+
+    water += waterToAdd;
+    milk += milkToAdd;
+    beans += beansToAdd;
+    cups += cupsToAdd;
+}
+
+function take(){
+    money = 0;
 }
 
 function main() {
-    const input = require('sync-input');
-    let waterInMachine = input("Write how many ml of water the coffee machine has:");
-    let milkInMachine = input("Write how many ml of milk the coffee machine has:");
-    let beansInMachine = input("Write how many grams of coffee beans the coffee machine has:");
-    let coffee = input("Write how many cups of coffee you will need:");
+    water = 400;
+    milk = 540;
+    beans = 120;
+    money = 550;
+    cups = 9;
 
-    let coffeeAvailable = calculateCoffeeAvailable(waterInMachine, milkInMachine, beansInMachine);
+    printState();
 
-    if (coffeeAvailable == coffee) {
-        console.log("Yes, I can make that amount of coffee");
-    } else if (coffeeAvailable > coffee) {
-        console.log(`Yes, I can make that amount of coffee (and even ${coffeeAvailable - coffee} more than that)`);
-    } else {
-        console.log(`No, I can make only ${coffeeAvailable} cups of coffee`);
-    }
+    let action = input("Write action (buy, fill, take):\n");
+
+    if (action == "buy") {
+        buy();
+    } else if (action == "fill") {
+        fill();
+    } else if (action == "take") {
+        take();
+    };
+
+    printState();
 }
 
 main();
